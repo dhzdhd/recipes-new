f139d084d099870d1ca34df7aa18963691219068
dhzdhd <dhzdhd@proton.me>
2026-07-21 11:59:46 +0530

    Init repository with Sveltia CMS integration

---
+++ src/routes/(admin)/admin/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { init, type CmsConfig } from '@sveltia/cms';
+
+	const config: CmsConfig = {
+		load_config_file: false,
+		backend: { name: 'test-repo' },
+		media_folder: 'static/media',
+		public_folder: '/media',
+		collections: [
+			{
+				name: 'posts',
+				label: 'Posts',
+				folder: 'content/posts',
+				fields: [
+					{ label: 'Title', name: 'title', widget: 'string' },
+					{ label: 'Date', name: 'date', widget: 'datetime' },
+					{ label: 'Body', name: 'body', widget: 'markdown' }
+				]
+			}
+		]
+	};
+
+	onMount(() => init({ config }));
+</script>
---
+++ src/routes/(app)/+page.svelte
@@ -0,0 +1,16 @@
+<header></header>
+<main class="h-dvh max-w-7xl bg-[#2C2314]">
+	<section id="hero"></section>
+	<section id="meta"></section>
+	<section id="instructions-and-ingredients">
+		<aside>
+			<h1>Ingredients</h1>
+		</aside>
+		<article>
+			<h1>Instructions</h1>
+			<p></p>
+		</article>
+	</section>
+	<section id="recommended"></section>
+</main>
+<footer></footer>
